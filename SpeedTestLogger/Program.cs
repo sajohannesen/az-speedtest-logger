f8fcab7614d709c11e015e9ba3fa189759346b91
Sara <sajo@computas.com>
2023-03-02 17:30:51 +0100

    My first SpeeTestLogger

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,19 @@
+﻿using SpeedTestLogger;
+using SpeedTestLogger.Models;
+
+Console.WriteLine("Hello, SpeedTestLogger!");
+
+var config = new LoggerConfiguration();
+var runner = new SpeedTestRunner(config.LoggerLocation);
+var testData = runner.RunSpeedTest();
+var results = new TestResult(
+    SessionId: Guid.NewGuid(),
+    User: config.UserId,
+    Device: config.LoggerId,
+    Timestamp: DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+    Data: testData
+    );
+
+
+
+
